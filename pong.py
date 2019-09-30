ea75aee587ee14c31fd0f3d24478839ac68953a7
jadenccw <goku7174@gmail.com>
2019-09-29 21:55:35 -0700

    Added Borders and Paddle Checking

--- pong.py
+++ pong.py
@@ -3,13 +3,14 @@
 
 import turtle
 
+# Screen Init
 wn = turtle.Screen()
 wn.title("Pong by Jaden Chung")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Paddle A
+# Paddle A Init
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -18,7 +19,7 @@ paddle_a.shapesize(stretch_wid= 5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-# Paddle B
+# Paddle B Init
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -27,13 +28,15 @@ paddle_b.shapesize(stretch_wid= 5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-# Ball
+# Ball Init
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.07
+ball.dy = 0.07
 
 # Paddle A Movement
 def paddle_a_up():
@@ -57,7 +60,6 @@ def paddle_b_down():
     y -= 20
     paddle_b.sety(y)
 
-
 # Keyboard Binding
 wn.listen() # Tells Window to Listen For Keyboard Input
 wn.onkeypress(paddle_a_up, "w") # On event (click w) call paddle_a_up
@@ -68,3 +70,39 @@ wn.onkeypress(paddle_b_down, "Down")
 # Main
 while True:
     wn.update()
+
+    player1score = 0
+    player2score = 0
+
+    # Ball Movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border Checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    # Paddle Checking
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    
